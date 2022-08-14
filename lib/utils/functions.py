e881d69bb32fec255f0a6982738c34dd9bfce1b9
Jose Armas <44787238+thyfus@users.noreply.github.com>
2022-08-14 19:12:15 -0400

    Add files via upload

--- lib/utils/functions.py
+++ lib/utils/functions.py
@@ -1,17 +1,37 @@
-#function to get the data from the channels
-def get_channels(youtube, channel_ids):
+import pandas as pd
+import numpy as np
+from dateutil import parser
+import isodate
 
-    """
-    Get channel stats: channelName, subscribers, views, videos, playlist
+# Data visualization libraries
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import seaborn as sns
+sns.set(style="darkgrid", color_codes=True)
+
+# Google API
+from googleapiclient.discovery import build
+
+# NLP libraries
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+nltk.download('stopwords')
+nltk.download('punkt')
+from wordcloud import WordCloud
+
+
+def get_channels(youtube, channel_ids):
     
-    Params:
-    ------
-    youtube: build object of Youtube API
-    channel_ids: list of channel IDs
+    """
+    Get channel statistics: title, subscriber count, view count, video count, upload playlist
+    
+    Params: 
+    youtube: the build object from googleapiclient.discovery
+    channels_ids: list of channel IDs
     
     Returns:
-    ------
-    dataframe with all channel stats for each channel ID
+    A dataframe containing the channel statistics
     
     """
     
@@ -21,20 +41,137 @@ def get_channels(youtube, channel_ids):
         part="snippet,contentDetails,statistics",
         id=','.join(channel_ids)
     )
-
     response = request.execute()
-
-    JSON(response)
     
     #loop through items
-    for item in response['items']:
-        data = {'channelName': item['snippet']['title'],
-               'subscribers': item['statistics']['subscriberCount'],
-                'views': item['statistics']['viewCount'],
-                'totalVideos': item['statistics']['videoCount'],
-                'playlist': item['contentDetails']['relatedPlaylists']['uploads']
-               }
-        
+    for i in range(len(response['items'])):
+        data = dict(channelName = response['items'][i]['snippet']['title'],
+                    subscribers = response['items'][i]['statistics']['subscriberCount'],
+                    views = response['items'][i]['statistics']['viewCount'],
+                    totalVideos = response['items'][i]['statistics']['videoCount'],
+                    playlistId = response['items'][i]['contentDetails']['relatedPlaylists']['uploads'])
         all_data.append(data)
         
     return(pd.DataFrame(all_data))
+
+
+def get_video_ids(youtube, playlist_id):
+    
+    """
+    Get list of video IDs of all videos in the playlist
+    
+    Params:
+    youtube: the build object from googleapiclient.discovery
+    playlist_id: playlist ID of the channel
+    
+    Returns:
+    List of video IDs
+    
+    """
+    
+    request = youtube.playlistItems().list(
+                part='contentDetails',
+                playlistId = playlist_id,
+                maxResults = 50)
+    response = request.execute()
+    
+    video_ids = []
+    
+    for i in range(len(response['items'])):
+        video_ids.append(response['items'][i]['contentDetails']['videoId'])
+        
+    next_page_token = response.get('nextPageToken')
+    more_pages = True
+    
+    while more_pages:
+        if next_page_token is None:
+            more_pages = False
+        else:
+            request = youtube.playlistItems().list(
+                        part='contentDetails',
+                        playlistId = playlist_id,
+                        maxResults = 50,
+                        pageToken = next_page_token)
+            response = request.execute()
+    
+            for i in range(len(response['items'])):
+                video_ids.append(response['items'][i]['contentDetails']['videoId'])
+            
+            next_page_token = response.get('nextPageToken')
+        
+    return video_ids
+
+
+def get_video_details(youtube, video_ids):
+    
+    """
+    Get video statistics of every video
+    
+    Params:
+    youtube: the build object from googleapiclient.discovery
+    video_ids: list of video IDs
+    
+    Returns:
+    Dataframe with statistics of videos
+    
+    """
+    
+    all_video_info = []
+    
+    for i in range(0, len(video_ids), 50):
+        request = youtube.videos().list(
+            part="snippet,contentDetails,statistics",
+            id=','.join(video_ids[i:i+50])
+        )
+        response = request.execute() 
+
+        for video in response['items']:
+            stats_to_keep = {'snippet': ['channelTitle', 'title', 'description', 'tags', 'publishedAt'],
+                             'statistics': ['viewCount', 'likeCount', 'favouriteCount', 'commentCount'],
+                             'contentDetails': ['duration', 'definition', 'caption']
+                            }
+            video_info = {}
+            video_info['video_id'] = video['id']
+
+            for k in stats_to_keep.keys():
+                for v in stats_to_keep[k]:
+                    try:
+                        video_info[v] = video[k][v]
+                    except:
+                        video_info[v] = None
+
+            all_video_info.append(video_info)
+            
+def get_comments(youtube, video_ids):
+    """
+    Get top level comments as text from all videos with given IDs (only the first 10 comments due to quote limit of Youtube API)
+    Params:
+    
+    youtube: the build object from googleapiclient.discovery
+    video_ids: list of video IDs
+    
+    Returns:
+    Dataframe with video IDs and associated top level comment in text.
+    
+    """
+    all_comments = []
+    
+    for video_id in video_ids:
+        try:   
+            request = youtube.commentThreads().list(
+                part="snippet,replies",
+                videoId=video_id
+            )
+            response = request.execute()
+            
+            #to not exceed the API limit, the number of comments was limited to the top ten
+            comments_in_video = [comment['snippet']['topLevelComment']['snippet']['textOriginal'] for comment in response['items'][0:10]]
+            comments_in_video_info = {'video_id': video_id, 'comments': comments_in_video}
+
+            all_comments.append(comments_in_video_info)
+            
+        except: 
+            # When error occurs - most likely because comments are disabled on a video
+            print('Could not get comments for video ' + video_id)
+        
+    return pd.DataFrame(all_comments)
